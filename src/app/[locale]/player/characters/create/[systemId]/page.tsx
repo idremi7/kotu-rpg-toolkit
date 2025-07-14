b464619073a7c8bd0583e4b32e8653db8f92704d
Rémi <remi.letourneau42@gmail.com>
2025-07-14 14:40:15 +0000

    dans le createur de personnage, tout est centrer, c'est tu vraiment mieu

--- src/app/[locale]/player/characters/create/[systemId]/page.tsx
+++ src/app/[locale]/player/characters/create/[systemId]/page.tsx
@@ -4,7 +4,7 @@ import { CharacterCreator } from "@/components/player/CharacterCreator";
 export default function CreateCharacterPage({ params }: { params: { systemId: string, locale: string }}) {
   return (
     <div className="container mx-auto px-4 py-8">
-      <div className="relative text-center mb-8">
+      <div className="relative mb-8">
          <div className="absolute left-0 top-1/2 -translate-y-1/2">
             <BackButton />
          </div>
